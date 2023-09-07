987e860b6c1ea87aaf16931b7bca5188e58808f2
Abimanyu Yuda Dewa <abimanyu@Abimanyus-MacBook-Pro.local>
2023-09-08 00:25:28 +0700

    Refactor: Added LinQ in SortService.cs

--- Qwiik_Casework/Services/SortService.cs
+++ Qwiik_Casework/Services/SortService.cs
@@ -6,9 +6,10 @@ namespace Qwiik_Casework.Services
 
         public int[] runSort()
         {
-            int[] l = new int[] { 3, 5, 8, 2, 4 };
-            return Quicksort.Sort(l, 0, l.Length-1);
-
+            int[] l = new int[] { 3, 5, 8, 2, 4, 10, 6, 16, 23, 9, 11, 24 };
+            var evenNums = from num in l where num % 2 == 0 select num;
+            var newL = evenNums.ToArray();
+            return Quicksort.Sort(newL, 0, newL.Length-1);
         }
     }
 }
